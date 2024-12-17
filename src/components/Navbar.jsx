fbd4b77657ade0f0032e82186a1f193675a4f02e
Agustin Velazquez <agubolso2@gmail.com>
2024-12-17 07:45:54 -0300

    better icons navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,12 @@ function NavBar() {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center"></div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      <div className="m-8 flex items-center justify-center gap-6 text-3xl">
         <a
           href="https://www.linkedin.com/in/agustin-vel%C3%A1zquez-173aaa1b5/"
           target="_blank"
           rel="noopener noreferrer"
+          className="text-[#0A66C2] hover:scale-110 transition-all duration-300"
         >
           <FaLinkedin />
         </a>
@@ -16,6 +17,7 @@ function NavBar() {
           href="https://github.com/AgustinVelazquez0"
           target="_blank"
           rel="noopener noreferrer"
+          className="text-[#333] hover:scale-110 transition-all duration-300"
         >
           <FaGithub />
         </a>
