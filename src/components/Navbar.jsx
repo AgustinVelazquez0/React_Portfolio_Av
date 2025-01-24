4ff32933fa8fa9f22106dd7af15dae47c7d16438
Agustin Velazquez <agubolso2@gmail.com>
2025-01-24 20:06:07 -0300

    refresh linkedin link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
       <div className="flex flex-shrink-0 items-center"></div>
       <div className="m-8 flex items-center justify-center gap-6 text-3xl">
         <a
-          href="https://www.linkedin.com/in/agustin-vel%C3%A1zquez-173aaa1b5/"
+          href="https://www.linkedin.com/in/agustin-vel%C3%A1zquez-dev/"
           target="_blank"
           rel="noopener noreferrer"
           className="text-[#0A66C2] hover:scale-110 transition-all duration-300"
