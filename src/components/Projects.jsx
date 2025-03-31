19953ef73778fb2264ec057f298a9ba877299656
Agustin Velazquez <agubolso2@gmail.com>
2025-03-31 13:59:50 -0300

    make 2 columns in projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,37 +17,43 @@ function Projects() {
       <h3 className="my-10 text-2xl text-center text-blue-500">
         React Projects
       </h3>
-      <div>
+      <div className="flex flex-wrap justify-center gap-12 px-6">
         {PROJECTS.REACT.map((project, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
-            >
-              <a href={project.link} target="_blank" rel="noopener noreferrer">
-                <img
-                  src={project.image}
-                  width={150}
-                  height={150}
-                  alt={project.title}
-                  className="mt-1 rounded"
-                />
-              </a>
-            </motion.div>
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100 }}
-              transition={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4"
-            >
-              <h6 className="mb-2 font-semibold">{project.title}</h6>
-              <p className="mb-4 text-neutral-400">{project.description}</p>
+          <motion.div
+            key={index}
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 50 }}
+            transition={{ duration: 0.5 }}
+            className="mb-12 w-full sm:w-full md:w-5/12 lg:w-5/12 flex flex-col bg-neutral-950 p-6 rounded-lg shadow-lg"
+          >
+            <div className="flex flex-wrap">
+              <div className="w-1/3">
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={project.image}
+                    width={150}
+                    height={150}
+                    alt={project.title}
+                    className="mt-1 rounded"
+                  />
+                </a>
+              </div>
+              <div className="w-2/3 pl-4">
+                <h6 className="mb-2 font-semibold">{project.title}</h6>
+                <p className="mb-4 text-neutral-400 text-sm">
+                  {project.description}
+                </p>
+              </div>
+            </div>
+            <div className="mt-auto">
               <div className="flex flex-wrap gap-2 mt-4">
-                {project.technologies.map((tech, index) => (
+                {project.technologies.map((tech, techIndex) => (
                   <span
-                    key={index}
+                    key={techIndex}
                     className="rounded bg-neutral-900 px-3 py-1 text-sm font-medium text-blue-600"
                   >
                     {tech}
@@ -61,13 +67,13 @@ function Projects() {
                   <>
                     <button
                       onClick={() => window.open(project.link, "_blank")}
-                      className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition duration-300"
+                      className="px-3 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition duration-300 text-sm"
                     >
                       View code (Frontend)
                     </button>
                     <button
                       onClick={() => window.open(project.github, "_blank")}
-                      className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition duration-300"
+                      className="px-3 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition duration-300 text-sm"
                     >
                       View code (Backend)
                     </button>
@@ -76,21 +82,21 @@ function Projects() {
                   <>
                     <button
                       onClick={() => window.open(project.link, "_blank")}
-                      className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition duration-300"
+                      className="px-3 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition duration-300 text-sm"
                     >
                       View project
                     </button>
                     <button
                       onClick={() => window.open(project.github, "_blank")}
-                      className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition duration-300"
+                      className="px-3 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition duration-300 text-sm"
                     >
                       View code
                     </button>
                   </>
                 )}
               </div>
-            </motion.div>
-          </div>
+            </div>
+          </motion.div>
         ))}
       </div>
 
@@ -98,37 +104,43 @@ function Projects() {
       <h3 className="my-10 text-2xl text-center text-yellow-500">
         JavaScript Projects
       </h3>
-      <div>
+      <div className="flex flex-wrap justify-center gap-12 px-6">
         {PROJECTS.JS.map((project, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
-            >
-              <a href={project.link} target="_blank" rel="noopener noreferrer">
-                <img
-                  src={project.image}
-                  width={150}
-                  height={150}
-                  alt={project.title}
-                  className="mt-1 rounded"
-                />
-              </a>
-            </motion.div>
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100 }}
-              transition={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4"
-            >
-              <h6 className="mb-2 font-semibold">{project.title}</h6>
-              <p className="mb-4 text-neutral-400">{project.description}</p>
+          <motion.div
+            key={index}
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 50 }}
+            transition={{ duration: 0.5 }}
+            className="mb-12 w-full sm:w-full md:w-5/12 lg:w-5/12 flex flex-col bg-neutral-950 p-6 rounded-lg shadow-lg"
+          >
+            <div className="flex flex-wrap">
+              <div className="w-1/3">
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={project.image}
+                    width={150}
+                    height={150}
+                    alt={project.title}
+                    className="mt-1 rounded"
+                  />
+                </a>
+              </div>
+              <div className="w-2/3 pl-4">
+                <h6 className="mb-2 font-semibold">{project.title}</h6>
+                <p className="mb-4 text-neutral-400 text-sm">
+                  {project.description}
+                </p>
+              </div>
+            </div>
+            <div className="mt-auto">
               <div className="flex flex-wrap gap-2 mt-4">
-                {project.technologies.map((tech, index) => (
+                {project.technologies.map((tech, techIndex) => (
                   <span
-                    key={index}
+                    key={techIndex}
                     className="rounded bg-neutral-900 px-3 py-1 text-sm font-medium text-yellow-600"
                   >
                     {tech}
@@ -139,19 +151,19 @@ function Projects() {
               <div className="mt-4 flex gap-2">
                 <button
                   onClick={() => window.open(project.link, "_blank")}
-                  className="px-4 py-2 bg-yellow-500 text-white rounded-md hover:bg-yellow-700 transition duration-300"
+                  className="px-3 py-1 bg-yellow-500 text-white rounded-md hover:bg-yellow-700 transition duration-300 text-sm"
                 >
                   View project
                 </button>
                 <button
                   onClick={() => window.open(project.github, "_blank")}
-                  className="px-4 py-2 bg-yellow-500 text-white rounded-md hover:bg-yellow-700 transition duration-300"
+                  className="px-3 py-1 bg-yellow-500 text-white rounded-md hover:bg-yellow-700 transition duration-300 text-sm"
                 >
                   View code
                 </button>
               </div>
-            </motion.div>
-          </div>
+            </div>
+          </motion.div>
         ))}
       </div>
 
@@ -159,37 +171,43 @@ function Projects() {
       <h3 className="my-10 text-2xl text-center text-green-500">
         HTML & CSS Projects
       </h3>
-      <div>
+      <div className="flex flex-wrap justify-center gap-12 px-6">
         {PROJECTS.HTML_CSS.map((project, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
-            >
-              <a href={project.link} target="_blank" rel="noopener noreferrer">
-                <img
-                  src={project.image}
-                  width={150}
-                  height={150}
-                  alt={project.title}
-                  className="mt-1 rounded"
-                />
-              </a>
-            </motion.div>
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100 }}
-              transition={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4"
-            >
-              <h6 className="mb-2 font-semibold">{project.title}</h6>
-              <p className="mb-4 text-neutral-400">{project.description}</p>
+          <motion.div
+            key={index}
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 50 }}
+            transition={{ duration: 0.5 }}
+            className="mb-12 w-full sm:w-full md:w-5/12 lg:w-5/12 flex flex-col bg-neutral-950 p-6 rounded-lg shadow-lg"
+          >
+            <div className="flex flex-wrap">
+              <div className="w-1/3">
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={project.image}
+                    width={150}
+                    height={150}
+                    alt={project.title}
+                    className="mt-1 rounded"
+                  />
+                </a>
+              </div>
+              <div className="w-2/3 pl-4">
+                <h6 className="mb-2 font-semibold">{project.title}</h6>
+                <p className="mb-4 text-neutral-400 text-sm">
+                  {project.description}
+                </p>
+              </div>
+            </div>
+            <div className="mt-auto">
               <div className="flex flex-wrap gap-2 mt-4">
-                {project.technologies.map((tech, index) => (
+                {project.technologies.map((tech, techIndex) => (
                   <span
-                    key={index}
+                    key={techIndex}
                     className="rounded bg-neutral-900 px-3 py-1 text-sm font-medium text-green-600"
                   >
                     {tech}
@@ -200,19 +218,19 @@ function Projects() {
               <div className="mt-4 flex gap-2">
                 <button
                   onClick={() => window.open(project.link, "_blank")}
-                  className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-700 transition duration-300"
+                  className="px-3 py-1 bg-green-500 text-white rounded-md hover:bg-green-700 transition duration-300 text-sm"
                 >
                   View project
                 </button>
                 <button
                   onClick={() => window.open(project.github, "_blank")}
-                  className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-700 transition duration-300"
+                  className="px-3 py-1 bg-green-500 text-white rounded-md hover:bg-green-700 transition duration-300 text-sm"
                 >
                   View code
                 </button>
               </div>
-            </motion.div>
-          </div>
+            </div>
+          </motion.div>
         ))}
       </div>
     </div>
