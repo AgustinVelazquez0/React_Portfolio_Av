a88c6034cc84bc6110da37e3572b4df5ebe5369f
Agustin Velazquez <agubolso2@gmail.com>
2025-05-16 20:24:12 -0300

    add translates

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,17 +1,11 @@
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import ThemeToggle from "../features/ThemeToggle";
 import LanguageToggle from "../features/LenguageToggle";
-import { useTranslation } from "../../hooks/useTranslation";
 
 function NavBar() {
-  const { t } = useTranslation(); // Usamos la función t para traducir
-
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 items-center">
-        {/* Si querés poner un título o algo traducido, acá iría: */}
-        {/* <h1 className="text-xl font-bold">{t("navbar_title")}</h1> */}
-      </div>
+      <div className="flex flex-shrink-0 items-center"></div>
 
       <div className="m-8 flex items-center justify-center gap-6 text-3xl">
         <a
